07651b92d29eaeed85c5cfa9cfea51eb9f2f41a7
fanzhong <fanzhongzeng78@gmail.com>
2020-01-31 16:45:20 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,20 @@
 export function manageFriends(state, action){
+	switch(action.type) {
+		case "ADD_FRIEND":
+			return {
+				...state,
+				friends: [...state.friends, action.friend]
+			}
+			break;
+		case "REMOVE_FRIEND":
+			let tempFriends = state.friends
+			
+			return {
+				...state,
+				friends: tempFriends.filter((friends) => {return friends.id !== action.id})
+			}
+			break;
+		default:
+			return state;
+	}
 }
